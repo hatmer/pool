f6e1a12b5af808dbfdfb79794977069134d1dff4
Hannah Atmer <ha237@cornell.edu>
2018-06-27 23:34:13 -0400

    initial commit

---
+++ common.ml
@@ -0,0 +1,14 @@
+
+
+
+(* push via plain websocket *)
+let push (msg : string) : bool =
+    (* retry forever if we fail *)
+
+    return deferred
+
+
+(* listening websocket server *)
+
+(* brute force sha256 *)
+
---
+++ miner.ml
@@ -0,0 +1,14 @@
+
+
+(* recv socket *)
+
+(* fxn to do hashing: set new value *)
+
+(* send result if found *)
+
+
+
+(* method for setting port, pool addr,  *)
+
+
+let server = Rpc.Server.create
