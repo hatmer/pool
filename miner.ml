c961fd32decdd39773b06d53296a5d3844dcf696
Hannah Atmer <ha237@cornell.edu>
2018-08-21 13:47:32 -0400

    https requests with error handling

--- miner.ml
+++ miner.ml
@@ -1,27 +1,19 @@
 open Cohttp
 open Async
 
-let url = "https://127.0.0.1:8000/fetch"
-let badurl = "https://127.0.0.1:8000/fe"
-let failurl = "hi"
-
+(* Make request and return response or status code *)
 let fetch (url_str : string) (name_str : string) =
   let url = Uri.of_string url_str in
   let headers = Cohttp.Header.prepend_user_agent (Cohttp.Header.of_list [ "connection", "close"]) name_str in
   let meth = Code.method_of_string "GET" in
-    Cohttp_async.Client.call meth ~headers url
+    try_with (fun () -> Cohttp_async.Client.call meth ~headers url
     >>= fun (res, body) -> 
     let http_code = Cohttp.Response.status res in 
     match http_code with
     | `OK -> Cohttp_async.Body.to_string body
-    | _ -> return "500"    
-;;
-
-
-let get_orders server_url = 
-  try_with (fun () -> fetch server_url "b")
-  >>| function
-    | Ok s -> s
-    | Error _ -> "400"
-;;
+    | _ -> return "400" (* bad url *)
+  ) >>| function    
+      | Ok s -> s
+      | Error _ -> "500" (* request failed *)
+;; 
 
