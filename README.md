00a31bb6a21576e928c008241d06ec8d4dfbfae1
Hannah <ha237@cornell.edu>
2019-12-22 18:10:09 +0000

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Mining Pool server with custom client (CPU miner) binary. Interfaces with either Bitcoin or Monero.
+Mining Pool server with custom OCaml CPU mining binary client. Interfaces with either Bitcoin or Monero.
+
+[![Watch the video](https://img.youtube.com/vi/rr0UxM84AII/maxresdefault.jpg)](https://youtu.be/rr0UxM84AII)
--- server/pool/helpers.py
+++ server/pool/helpers.py
@@ -13,7 +13,6 @@ class Task_selector: # TODO run simulations of this algorithm, add to paper
         self.workers_served = 0
 
     def next(self, ID):
-
         self.workers_served += 1
         if self.workers_served > self.num_clients_last_round: # served all intervals
             self.num -= self.step / 2 # stagger the intervals
@@ -21,7 +20,6 @@ class Task_selector: # TODO run simulations of this algorithm, add to paper
             self.step *= -1 # go back in the opposite direction
 
         self.num += self.step
-
         return self.num % MAX_NONCE
 
 
